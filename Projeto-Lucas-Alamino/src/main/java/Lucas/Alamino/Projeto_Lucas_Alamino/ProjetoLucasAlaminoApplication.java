7988d0670e542bcde5eddd761ac7dca907afabe9
Lucas Alamino <alaminolucas@gmail.com>
2024-07-26 15:11:36 -0300

    Inicio do projeto

---
+++ Projeto-Lucas-Alamino/src/main/java/Lucas/Alamino/Projeto_Lucas_Alamino/ProjetoLucasAlaminoApplication.java
@@ -0,0 +1,13 @@
+package Lucas.Alamino.Projeto_Lucas_Alamino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLucasAlaminoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoLucasAlaminoApplication.class, args);
+	}
+
+}
---
+++ Projeto-Lucas-Alamino/src/test/java/Lucas/Alamino/Projeto_Lucas_Alamino/ProjetoLucasAlaminoApplicationTests.java
@@ -0,0 +1,13 @@
+package Lucas.Alamino.Projeto_Lucas_Alamino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLucasAlaminoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
